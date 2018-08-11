7a95762e2e8ecfc52f2b3a30f1bdd504613357ee
denissurkov <surkov0denis@yandex.ru>
2018-08-11 14:35:33 +0500

    add nginx restart

--- wsgi_app_server/init.sh
+++ wsgi_app_server/init.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
+
+sudo ln -sf /home/box/web/base.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
