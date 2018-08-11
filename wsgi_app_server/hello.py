b17d3dff2da22c13577631bc6bf66c905695c74a
denissurkov <surkov0denis@yandex.ru>
2018-08-11 15:05:44 +0500

    add '/' to nginx and updated ans comp

--- wsgi_app_server/hello.py
+++ wsgi_app_server/hello.py
@@ -2,11 +2,10 @@
 
 def main_app(environ, start_response):
     data = environ.get('QUERY_STRING')
-    answer = '\n'.join(data.split('&'))
+    answer = [bytes(i + '\n', 'ascii') for i in data.split('&')]
     status = '200 OK'
     response_headers = [
-        ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(data)))
+        ('Content-Type', 'text/plain')
     ]
     start_response(status, response_headers)
     return answer
